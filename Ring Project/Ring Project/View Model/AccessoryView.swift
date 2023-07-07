9666135f61230de58338208018e06eab29879ebc
LucasL53 <57372991+LucasL53@users.noreply.github.com>
2023-07-06 21:55:25 -0700

    Access Airplay feature

--- Ring Project/Ring Project/View Model/AccessoryView.swift
+++ Ring Project/Ring Project/View Model/AccessoryView.swift
@@ -9,38 +9,49 @@ struct AccessoriesView: View {
     @State private var selectedAccessoryId: UUID?
 
     var body: some View {
-        VStack{
-            ZStack{
-                ScrollView{
-                    Section(header: Text("My Accessories")){
-                        VStack{
-                            ScrollView(.horizontal){
-                                HStack(spacing: 16){
-                                    ForEach(model.accessories, id: \.uniqueIdentifier) { accessory in
-                                        SelectButton(isSelected:
-                                                        Binding(
-                                                            get: { self.selectedAccessory ?? "none" },
-                                                            set: { self.selectedAccessory = $0}
-                                                        )
-                                                     , color: .blue, text: "\(accessory.name)")
-                                        .onTapGesture {
-                                            selectedAccessory = "\(accessory.name)"
-                                            selectedAccessoryId = accessory.uniqueIdentifier
-                                        }
-                                        .padding()
+        ZStack{
+            ScrollView{
+                Section(header: Text("My Accessories")){
+                    VStack{
+                        ScrollView(.horizontal){
+                            HStack(spacing: 16){
+                                ForEach(model.accessories, id: \.uniqueIdentifier) { accessory in
+                                    SelectButton(isSelected:
+                                                    Binding(
+                                                        get: { self.selectedAccessory ?? "none" },
+                                                        set: { self.selectedAccessory = $0}
+                                                    )
+                                                 , color: .blue, text: "\(accessory.name)")
+                                    .onTapGesture {
+                                        selectedAccessory = "\(accessory.name)"
+                                        selectedAccessoryId = accessory.uniqueIdentifier
                                     }
+                                    .padding()
                                 }
-                            }.onAppear(){model.findAccessories(homeId: homeId)}
-                            if let selectedAccessoryId = selectedAccessoryId {
-                                ServicesView(accessoryId: selectedAccessoryId, homeId: homeId, model: model)
+//                                AirPlayView()
+//                                    .frame(width: 100, height: 50) // adjust as needed
+//                                    .background(Color.blue)
+//                                    .clipShape(Circle())
+//                                    .onTapGesture {
+//                                        // handle selection of AirPlay device here...
+//                                    }
+//                                    .padding()
+                                RoutePickerView()
+                                    .frame(width: 100, height: 50) // adjust as needed
+                                    .background(Color.blue)
+                                    .clipShape(Circle())
+                                    .padding()
+                                
                             }
+                        }.onAppear(){model.findAccessories(homeId: homeId)}
+                        if let selectedAccessoryId = selectedAccessoryId {
+                            ServicesView(accessoryId: selectedAccessoryId, homeId: homeId, model: model)
                         }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .edgesIgnoringSafeArea(.all)
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .edgesIgnoringSafeArea(.all)
                 }
             }
-//            CameraView()
         }
     }
 }
---
+++ Ring Project/Ring Project/View Model/RoutePickerView.swift
@@ -0,0 +1,22 @@
+//
+//  RoutePickerView.swift
+//  Ring Project
+//
+//  Created by Yunseo Lee on 7/6/23.
+//
+
+import SwiftUI
+import AVKit
+
+struct RoutePickerView: UIViewRepresentable {
+    func makeUIView(context: Context) -> AVRoutePickerView {
+        let picker = AVRoutePickerView()
+        picker.activeTintColor = .blue
+        picker.tintColor = .blue
+        return picker
+    }
+    
+    func updateUIView(_ uiView: AVRoutePickerView, context: Context) {
+        // No update needed
+    }
+}
