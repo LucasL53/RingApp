3af2315f1a469cd0a6a0be1a9b6fb448621e2a8a
LucasL53 <57372991+LucasL53@users.noreply.github.com>
2024-04-01 09:44:55 -0700

    UI Final

--- Ring-Camera-Control/Ring-Camera-Control/View/ControlView.swift
+++ Ring-Camera-Control/Ring-Camera-Control/View/ControlView.swift
@@ -30,140 +30,156 @@ struct ControlView: View {
     @State private var tempBool: Bool = false
 
     var body: some View {
-        VStack{
-            ScrollView {
-                Section (header: Text("IRIS")
-                    .bold()
-                    .font(.title) // Increase the font size
-                    .frame(alignment: .leading))
-                {
-                    Spacer()
-                        
-                        if tempBool {
-                            Image("RingLighter")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 100, height: 100)
-                                    .accessibilityLabel("Ring connected symbol")
-                                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                                    .onTapGesture {
-                                        tempBool.toggle()
-                                    }
-                            Text("IRIS Connected")
-                                .font(.caption)
-                        } else {
-                            Image("RingDarker")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 100, height: 100)
-                                    .accessibilityLabel("Ring disconnected symbol")
-                                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                                    .onTapGesture {
-                                        tempBool.toggle()
-                                    }
-                            Text("Searching for IRIS")
-                                .font(.caption)
-                        }
-                    
-                }
-                Spacer()
-//                HStack{
-//                    RouterPicker()
-//                        .frame(width: 100, height: 50) // adjust as needed
-//                        .background(Color.blue)
-//                        .clipShape(RoundedRectangle(cornerRadius: 25.0))
-//                        .padding()
-//                }
-                
-//                Spacer()
-//                Section(header: Text("My Smart Home Device")
-//                    .frame(maxWidth: .infinity, alignment: .leading)) {
-//                    Picker("My Smart Home Device", selection: $selectedAccessoryId){
-//                        ForEach(exampleClass.allCases) { category in
-//                             Text(category.rawValue).tag(category)
-//                       }
-//                    }
-//                    .pickerStyle(.segmented)
-//                    .onReceive(blemanager.$prediction) { newPrediction in
-//                        if let newPrediction = newPrediction {
-//                            selectedAccessoryId = newPrediction
-//                        }
-//                    }
-//                }
-//                Spacer()
-                HStack {
-                    Spacer(minLength: 20)
-                    
-                    VStack {
-                        Section(header: Text("Live View")
-                            .bold()
-                            .font(.system(size: 22))
-                            .frame(maxWidth: .infinity, alignment: .leading)) {
-                            ZStack {
-                                Rectangle()
-                                    .fill(Color.gray)
-                                    .frame(height: 480)
-                                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                                    .accessibilityLabel("Live Video Feed")
+            VStack{
+                ScrollView {
+                    HStack {
+                        Spacer(minLength: 20)
+                        VStack {
+                            Section (header: Text("IRIS")
+                                .bold()
+                                .font(.title) // Increase the font size
+                                .frame(maxWidth: .infinity, alignment: .leading))
+                            {
+                                Spacer()
                                 
-                                // Foreground image if available
-                                if let image = blemanager.thisImage {
-        //                            GeometryReader { proxy in
-        //                                image
-        //                                    .resizable()
-        //                                    .scaledToFit()
-        //                                    .frame(width: proxy.size.width * 0.95)
-        //                                    .frame(width: proxy.size.width, height: proxy.size.height)
-        //                            }
-                                    image
-                                    .resizable()
+                                if tempBool {
+                                    VStack {
+                                        Image("RingLighter")
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 100, height: 100, alignment: .leading)
+                                            .accessibilityLabel("Ring connected symbol")
+                                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                                            .onTapGesture {
+                                                tempBool.toggle()
+                                            }
+                                        Text("IRIS Connected")
+                                            .font(.caption)
+                                            .frame(alignment: .leading)
+                                    }.frame(maxWidth: .infinity, alignment: .leading)
+                                    
+                                } else {
+                                    VStack {
+                                        Image("RingDarker")
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 100, height: 100, alignment: .leading)
+                                            .accessibilityLabel("Ring disconnected symbol")
+                                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                                            .onTapGesture {
+                                                tempBool.toggle()
+                                            }
+                                        Text("Searching for IRIS")
+                                            .font(.caption)
+                                            .frame(alignment: .leading)
+                                    }.frame(maxWidth: .infinity, alignment: .leading)
+                                    
                                 }
+                                
                             }
                         }
+                        
                     }
                     
+                    Spacer()
+                    //                HStack{
+                    //                    RouterPicker()
+                    //                        .frame(width: 100, height: 50) // adjust as needed
+                    //                        .background(Color.blue)
+                    //                        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                    //                        .padding()
+                    //                }
                     
-                    Spacer(minLength: 20)
+                    //                Spacer()
+                    //                Section(header: Text("My Smart Home Device")
+                    //                    .frame(maxWidth: .infinity, alignment: .leading)) {
+                    //                    Picker("My Smart Home Device", selection: $selectedAccessoryId){
+                    //                        ForEach(exampleClass.allCases) { category in
+                    //                             Text(category.rawValue).tag(category)
+                    //                       }
+                    //                    }
+                    //                    .pickerStyle(.segmented)
+                    //                    .onReceive(blemanager.$prediction) { newPrediction in
+                    //                        if let newPrediction = newPrediction {
+                    //                            selectedAccessoryId = newPrediction
+                    //                        }
+                    //                    }
+                    //                }
+                    //                Spacer()
+                    HStack {
+                        Spacer(minLength: 20)
+                        
+                        VStack {
+                            Section(header: Text("Live View")
+                                .bold()
+                                .font(.system(size: 22))
+                                .frame(maxWidth: .infinity, alignment: .leading)) {
+                                    ZStack {
+                                        Rectangle()
+                                            .fill(Color.gray)
+                                            .frame(height: 480)
+                                            .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                                            .accessibilityLabel("Live Video Feed")
+                                        
+                                        // Foreground image if available
+                                        if let image = blemanager.thisImage {
+                                            //                            GeometryReader { proxy in
+                                            //                                image
+                                            //                                    .resizable()
+                                            //                                    .scaledToFit()
+                                            //                                    .frame(width: proxy.size.width * 0.95)
+                                            //                                    .frame(width: proxy.size.width, height: proxy.size.height)
+                                            //                            }
+                                            image
+                                                .resizable()
+                                        }
+                                    }
+                                }
+                        }
+                        
+                        
+                        Spacer(minLength: 20)
+                    }
+                    
+                    Spacer(minLength: 30)
+                    
+                    //                Button(action: {
+                    //                    blemanager.savePicture()
+                    //                }) {
+                    //                    Text("Save Picture")
+                    //                        .frame(width: 110, height: 10)
+                    //                }
+                    //                .buttonStyle(OutlinedButtonStyle())
+                    //                Spacer(minLength: 30)
+                    //
+                    //                Button(action: {
+                    //                    model.toggleAccessory(accessoryIdentifier: UUID(uuidString: model.homeDictionary["lights"]!)!)
+                    //                }) {
+                    //                    Text("Toggle Bulb")
+                    //                        .frame(width: 110, height: 10)
+                    //                }
+                    //                .buttonStyle(OutlinedButtonStyle())
+                    //                Spacer(minLength: 30)
+                    //
+                    //
+                    //                if spotify {
+                    //                    Image(systemName: "pause.fill")
+                    //                        .font(.title)
+                    //                        .onTapGesture {
+                    //                            musicModel.pause()
+                    //                            spotify = false
+                    //                        }
+                    //                } else {
+                    //                    Image(systemName: "play.fill")
+                    //                        .font(.title)
+                    //                        .onTapGesture {
+                    //                            musicModel.resumePlayback()
+                    //                            spotify = true
+                    //                        }
+                    //                }
                 }
-                
-                Spacer(minLength: 30)
-                
-//                Button(action: {
-//                    blemanager.savePicture()
-//                }) {
-//                    Text("Save Picture")
-//                        .frame(width: 110, height: 10)
-//                }
-//                .buttonStyle(OutlinedButtonStyle())
-//                Spacer(minLength: 30)
-//
-//                Button(action: {
-//                    model.toggleAccessory(accessoryIdentifier: UUID(uuidString: model.homeDictionary["lights"]!)!)
-//                }) {
-//                    Text("Toggle Bulb")
-//                        .frame(width: 110, height: 10)
-//                }
-//                .buttonStyle(OutlinedButtonStyle())
-//                Spacer(minLength: 30)
-//
-//
-//                if spotify {
-//                    Image(systemName: "pause.fill")
-//                        .font(.title)
-//                        .onTapGesture {
-//                            musicModel.pause()
-//                            spotify = false
-//                        }
-//                } else {
-//                    Image(systemName: "play.fill")
-//                        .font(.title)
-//                        .onTapGesture {
-//                            musicModel.resumePlayback()
-//                            spotify = true
-//                        }
-//                }
             }
-        }.onChange(of: blemanager.banjiStatus) {
+        .onChange(of: blemanager.banjiStatus) {
             blemanager.scanForPeripherals()
             blemanager.setHomeStore(homeStore: model)
         }
